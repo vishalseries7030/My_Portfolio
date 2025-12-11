61ac0f967847e69e448c0d8d2a40ba8e9176a1cc
Vishal Payyawar <vishal.payyawar22@vit.edu>
2025-12-11 10:43:36 +0530

    Fix ESLint config for Vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+export default [
+  js.configs.recommended,
+  nextPlugin.configs["core-web-vitals"],
+  {
+    rules: {
+      // add custom overrides if needed
+      "react/jsx-no-undef": "off",
+      "no-unused-vars": "warn",
+    },
+  },
+];
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default eslintConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
+
